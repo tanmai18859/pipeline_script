5762ea0ed3ea50bd1447197752354434a9c26732
tanmai18859 <tanamigorantla@gmail.com>
2026-08-03 07:02:52 +0530

    Create deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ==========================================
+echo DEPLOYMENT STAGE STARTED
+echo ==========================================
+echo Deploying the Application...
+timeout /t 3 >nul
+echo Deployment Completed Successfully.
