29a24a4fe25ff337493daf0d2a87b1248824909c
tanmai18859 <tanamigorantla@gmail.com>
2026-08-03 11:57:46 +0530

    Update quality.bat

--- quality.bat
+++ quality.bat
@@ -3,5 +3,4 @@ echo ==========================================
 echo CODE QUALITY STAGE STARTED
 echo ==========================================
 echo Checking Code Quality...
-timeout /t 3 >nul
 echo Quality Check Passed.
