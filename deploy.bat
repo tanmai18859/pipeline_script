13c9ef926cc9ded917bd2c53f084a7921ab0f676
tanmai18859 <tanamigorantla@gmail.com>
2026-08-03 11:57:22 +0530

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -3,5 +3,4 @@ echo ==========================================
 echo DEPLOYMENT STAGE STARTED
 echo ==========================================
 echo Deploying the Application...
-timeout /t 3 >nul
 echo Deployment Completed Successfully.
