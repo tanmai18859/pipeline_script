72541380e94a5dd48c744263276e5b466d918a61
tanmai18859 <tanamigorantla@gmail.com>
2026-08-03 06:59:58 +0530

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ==========================
+echo BUILD STAGE STARTED
+echo ==========================
+echo Compiling Application...
+timeout /t 3 >nul
+echo Build Completed Successfully.
