77453363f6c57e1e6b02713cb3816ad166ba359a
tanmai18859 <tanamigorantla@gmail.com>
2026-08-03 11:58:54 +0530

    Update unit.bat

--- unit.bat
+++ unit.bat
@@ -3,5 +3,4 @@ echo ==========================================
 echo UNIT TEST STAGE STARTED
 echo ==========================================
 echo Running Unit Tests...
-timeout /t 3 >nul
 echo All Unit Tests Passed Successfully.
